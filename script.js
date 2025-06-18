3453d8518a5106b66d52af6461affa7158522a6c
TristanEDU <tristanedu@protonmail.com>
2025-06-18 15:44:38 -0700

    work in progress on the tv iframe display.

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var buttons = document.querySelectorAll(".television__channel a");
+console.log(buttons);
 for (var i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener("click", function (event) {
+    console.log("Button clicked:", this.href);
     document.querySelector(".television__screen iframe").src = this.href;
     event.preventDefault();
   });
